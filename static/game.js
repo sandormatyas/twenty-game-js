3b484409da387994de47fac4fe22d29a0ef6a16c
Ben Polgardy <benpolgardy@gmail.com>
2019-10-21 19:38:20 +0200

    add isIterable() function

--- static/game.js
+++ static/game.js
@@ -1,14 +1,24 @@
+const isIterable = (obj) => {
+    if (obj == null) {
+        return false;
+    }
+    return typeof obj[Symbol.iterator] === 'function';
+}
+
+
 const addEventListenerTo = (object, event, callback) => {
     for (const element of object) {
         element.addEventListener(event, callback);
     }
 }
 
+
 const getCells = () => {
     const cells = document.querySelectorAll('.cell');
     console.log(cells)
     return cells;
 }
+
 
 const handleClick = () => {
     console.log('hello');
