468ac1118eb56193d8c03b65c0a53f978a50de14
sandormatyas <sandormatyas7@gmail.com>
2019-10-22 14:23:28 +0200

    implemented push rows

--- static/js/game.js
+++ static/js/game.js
@@ -6,11 +6,25 @@ function addDragulaToElements() {
 }
 
 
+function setCoinCoord(coin) {
+    coin.dataset.row = coin.parentNode.dataset.row;
+    coin.dataset.col = coin.parentNode.dataset.col;
+}
+
+
 function shiftBlocksUp() {
     let coins = document.querySelectorAll('.coin');
     for (let coin of coins) {
+        let newXCoord = coin.dataset.row -1;
+        let YCoord = coin.dataset.col;
 
+        let fragment = document.createDocumentFragment();
+        fragment.appendChild(coin);
+        document.querySelector(`.cell[data-row="${newXCoord}"][data-col="${YCoord}"]`).appendChild(fragment);
+
+        setCoinCoord(coin);
     }
+
 }
 
 
