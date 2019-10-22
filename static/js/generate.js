61f860240b8405055d2e620ff688d3f7866f3c68
botond <belgianmaniax@gmail.com>
2019-10-22 12:52:03 +0200

    implement generateRow

---
+++ static/js/generate.js
@@ -0,0 +1,17 @@
+function Board() {
+    this.height = 8;
+    this.maxnumber = 5;
+}
+
+function generateRow() {
+    const cells = document.querySelectorAll('.cell');
+    const board = new Board();
+    for (const cell of cells) {
+        if (Number(cell.dataset.row) === board.height - 1) {
+            const number = Math.floor(Math.random() * (board.maxnumber + 1));
+            cell.textContent = number.toString();
+        }
+    }
+}
+
+generateRow();
