6db927973c86fec923dde16d83b8c36363616599
RolandOver9000 <csosz.roland2@gmail.com>
2019-10-22 16:30:55 +0200

    sum cell numbers

--- static/js/game.js
+++ static/js/game.js
@@ -1,13 +1,26 @@
 
 
+function mergeCells(dragulaElements) {
+    let drake = dragulaElements;
+    drake.on("drop",  function (el, target, source, sibling) {
+        console.log(target);
+        target.innerText = Number(target.innerText) + Number(source.innerText);
+    })
+}
+
 function addDragulaToElements() {
     const cells = Array.from(document.querySelectorAll(".cell"));
     dragula(cells);
+    let dragulaElements = dragula(cells);
+
+    return dragulaElements;
+
 }
 
 
 function main() {
-    addDragulaToElements();
+    dragulaElemenets = addDragulaToElements();
+    mergeCells(dragulaElemenets);
 }
 
 
