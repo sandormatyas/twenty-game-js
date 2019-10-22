33b0dded7891e8079f58ffee8127a03ea6bf36f1
Ben Polgardy <benpolgardy@gmail.com>
2019-10-22 12:55:06 +0200

    add getCellCoordinates() & getAdjacentCellsFor(cell) functions

--- static/js/game.js
+++ static/js/game.js
@@ -65,13 +65,47 @@ const fieldIsTaken = () => {
 
 
 const handleClick = () => {
-    console.log('clicked');
     if (elementIsDraggable(event.target)) {
         setNotDraggable(event.target);
     } else {
         setDraggable(event.target);
+    }
+    /*getAdjacentCellsFor(event.target);*/
 }
 
+
+const getCellCoordinates = (cell) => {
+    const coordinates = []
+    const x = cell.dataset.coordinateX;
+    const y = cell.dataset.coordinateY;
+    coordinates.push(Number(x));
+    coordinates.push(Number(y));
+    return coordinates;
+}
+
+
+const getAdjacentCellsFor = (cell) => {
+    const elementCoordinates = getCellCoordinates(cell);
+    const elementX = elementCoordinates[0];
+    const elementY = elementCoordinates[1];
+    const rowAbove = elementY - 1;
+    const sameRow = elementY;
+    const rowBelow = elementY + 1;
+    const previousColumn = elementX - 1;
+    const sameColumn = elementX;
+    const nextColumn = elementX + 1;
+    const adjacentCells = [
+        [previousColumn, rowAbove],
+        [sameColumn, rowAbove],
+        [nextColumn, rowAbove],
+        [previousColumn, sameRow],
+        [nextColumn, sameRow],
+        [previousColumn, rowBelow],
+        [sameColumn, rowBelow],
+        [nextColumn, rowBelow]
+    ]
+    return adjacentCells;
+}
 
 
 const main = () => {
