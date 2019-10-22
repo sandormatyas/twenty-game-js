1dc014b7e9aad2726782aa28cad21a17fa4f7389
botond <belgianmaniax@gmail.com>
2019-10-22 20:48:34 +0200

    align numbers to the center of their parent coins

--- static/js/generate.js
+++ static/js/generate.js
@@ -9,7 +9,7 @@ function generateRow() {
     for (const cell of cells) {
         if (Number(cell.dataset.row) === board.height - 1) {
             const number = Math.floor(Math.random() * board.maxnumber) + 1;
-            cell.innerHTML = `<div class="coin">${number}</div>`
+            cell.innerHTML = `<div class="coin"><div class="number">${number}</div></div>`
         }
     }
 }
