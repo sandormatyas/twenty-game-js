17c8070db3f77798d4f40388ca7918a64d8c96bb
Ben Polgardy <benpolgardy@gmail.com>
2019-10-23 21:40:18 +0200

    fix bug in setMutationObserver() function & move code to generate.js

--- static/js/generate.js
+++ static/js/generate.js
@@ -1,4 +1,123 @@
-//---------------RESET GAME-----------------------------------------------------
+// ---------------SET ATTRIBUTE OF SURROUNDED BLOCKS-----------------------------
+function getCellCoordinates(cell) {
+    const coordinates = [];
+    const x = cell.dataset.col;
+    const y = cell.dataset.row;
+    coordinates.push(Number(x));
+    coordinates.push(Number(y));
+    return coordinates;
+}
+
+
+function getCellByCoordinates(coordinateX, coordinateY) {
+    return document.querySelector(`.cell[data-col="${coordinateX}"][data-row="${coordinateY}"]`);
+}
+
+
+function getCoins() {
+    const coins = document.querySelectorAll('.coin');
+    return coins;
+}
+
+
+function coinBlocked(coin) {
+    const adjacentCellValues = getContentOfAdjacentCellsFor(coin);
+    if (adjacentCellValues.includes('')) {
+        return false;
+    }
+    return true;
+}
+
+
+function setBlockedAttributeOf(coins) {
+    for (const coin of coins) {
+        if (coinBlocked(coin)) {
+            setBlocked(coin);
+        } else {
+            setNotBlocked(coin);
+        }
+    }
+}
+
+
+function setBlocked(element) {
+    element.dataset.blocked = 'true';
+}
+
+
+function setNotBlocked(element) {
+    element.dataset.blocked = 'false';
+}
+
+
+function getContentOfAdjacentCellsFor(coin) {
+    const adjacentCells = getAdjacentCellsFor(coin);
+    const contentOfAdjacentCells = [];
+    for (const adjacentCell of adjacentCells) {
+        const adjacentCellContent = getCellByCoordinates(adjacentCell[0], adjacentCell[1]).textContent;
+        contentOfAdjacentCells.push(adjacentCellContent);
+    }
+    return contentOfAdjacentCells;
+}
+
+
+function getAdjacentCellsFor(element) {
+    const elementCoordinates = getCellCoordinates(element);
+    const elementX = elementCoordinates[0];
+    const elementY = elementCoordinates[1];
+    const rowAbove = elementY - 1;
+    const sameRow = elementY;
+    const rowBelow = elementY + 1;
+    const previousColumn = elementX - 1;
+    const sameColumn = elementX;
+    const nextColumn = elementX + 1;
+    const adjacentCells = [
+        [sameColumn, rowAbove],
+        [previousColumn, sameRow],
+        [nextColumn, sameRow],
+        [sameColumn, rowBelow]
+    ]
+    const adjacentCellsPurged = purgeOutOfBound(adjacentCells);
+    return adjacentCellsPurged;
+}
+
+
+function purgeOutOfBound(coordinate_pairs) {
+    const purgedCoordinates = [];
+    for (const coordinate_pair of coordinate_pairs) {
+        if (
+            (coordinate_pair[0] < 0 || coordinate_pair[0] > 6) ||
+            (coordinate_pair[1] < 0 || coordinate_pair[1] > 7)
+        ) {
+            continue;
+        } else {
+            purgedCoordinates.push(coordinate_pair);
+        }
+    }
+    return purgedCoordinates;
+}
+
+
+function setMutationObserver() {
+    const targetNode = document.querySelector('#main-game-board');
+    const config = {attributes: true, childList: false, subtree: true};
+    const callback = function(mutationsList) {
+        for (const mutation of mutationsList) {
+            if (mutation.attributeName === 'data-row' ||
+                mutation.attributeName === 'data-col'
+            ) {
+                const coins = getCoins();
+                setBlockedAttributeOf(coins);
+                break;
+            }
+        }
+    };
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+};
+
+
+// ---------------RESET GAME-----------------------------------------------------
 function resetGame() {
     const cells = document.querySelectorAll(".cell");
     for (let cell of cells) {
@@ -72,7 +191,6 @@ function generateRow() {
 function handleRows() {
     const timerBar = document.getElementById('timer-bar');
     const timesUp = new Event('timesUp');
-
     timerBar.addEventListener('timesUp', function (event) {
         if (loseCheck()) {
             resetGame()
@@ -84,7 +202,7 @@ function handleRows() {
 
 
         let width = 100;
-        let timeHandler = setInterval(decreaseTime, 200);
+        let timeHandler = setInterval(decreaseTime, 15);
 
         function decreaseTime() {
             if (width <= 0) {
@@ -105,6 +223,7 @@ function handleRows() {
 function main() {
     addDragulaToElements();
     handleRows();
+    setMutationObserver();
 }
 
 main();
